85b0357edced2ad46ead331a447573abea184091
Ericlys Moreira <ericlysm@gmail.com>
2023-05-12 08:35:18 -0300

    feat: onesignal and pre build

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'react-native';
 import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
@@ -9,9 +10,7 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-console.log(process.env.ONE_SIGNAL_ID)
-
-OneSignal.setAppId(process.env.ONE_SIGNAL_ID!);
+OneSignal.setAppId(process.env.ONESIGNAL_ID!)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
