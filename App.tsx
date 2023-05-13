8cd42970dd0bb9662cd8af896e340fc665aad436
Ericlys Moreira <ericlysm@gmail.com>
2023-05-13 12:39:14 -0300

    chore: test segment email in onesignal

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'react-native';
 import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ONESIGNAL_ID } from '@env';
 
 import { Routes } from './src/routes';
 
@@ -10,7 +11,9 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-OneSignal.setAppId(process.env.ONESIGNAL_ID!)
+OneSignal.setAppId(ONESIGNAL_ID);
+
+OneSignal.setEmail('ericlysm@gmail.com'); 
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
